6a8a649a4de8b5f38391c75be7ec8ed1c7c5fd5d
Luciano Alicata <LucianoAntonioAlicata@alu.frt.utn.edu.ar>
2026-05-03 11:47:50 -0200

    problema 1

---
+++ Dsw2026Ej8/ProductHelperAux.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static partial class ProductHelper
+    {
+        private static string FormatearPrecio(decimal price)
+        {
+            return price.ToString("C", CultureInfo.CurrentCulture);
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string etiqueta = ProductHelper.ObtenerEtiquetaProducto(77881, "camisa", 10000);
+            Console.WriteLine(etiqueta);
         }
     }
 }
